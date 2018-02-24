467fdc25dcb0e146d325f45a18eabf14e4e5d514
hpan <hpan@thoughtworks.com>
2018-02-24 10:07:15 +0800

    Updates

---
+++ docs/test/unit/setup.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _vue = require('vue');
+
+var _vue2 = _interopRequireDefault(_vue);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_vue2.default.config.productionTip = false;
+//# sourceMappingURL=setup.js.map
