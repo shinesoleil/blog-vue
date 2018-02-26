c36dd6c0acebb05d842f9b7ccd38d77a0e4fe74e
hpan <hpan@thoughtworks.com>
2018-02-26 11:19:26 +0800

    Updates

---
+++ docs/docs/docs/docs/docs/config/prod.env.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  NODE_ENV: '"production"'
+};
+//# sourceMappingURL=prod.env.js.map
+//# sourceMappingURL=prod.env.js.map
+//# sourceMappingURL=prod.env.js.map
+//# sourceMappingURL=prod.env.js.map
+//# sourceMappingURL=prod.env.js.map
