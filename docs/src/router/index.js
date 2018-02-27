e4f479b219212b9135b22b68f096d708b9dbff2c
hpan <hpan@thoughtworks.com>
2018-02-27 16:41:31 +0800

    Updates

--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -42,7 +42,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 _vue2.default.use(_vueRouter2.default);
 
 exports.default = new _vueRouter2.default({
-  mode: 'history',
   routes: [{
     path: '/',
     redirect: '/blogs'
