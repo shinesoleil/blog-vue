96b42e097e6cfe6d8be8f460a6acc8720da6b8ac
hpan <hpan@thoughtworks.com>
2018-02-27 14:53:53 +0800

    blog date

--- src/components/blog/BlogList.vue
+++ src/components/blog/BlogList.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <h1>The Blogs</h1>
-    <ul v-for="info in infos" v-bind:key="info.title.toString()">
-      <li @click="handleClick(info.id)">{{info.title}}</li>
+    <h1>Blogs Catalog</h1>
+    <ul class="list">
+      <li class="item"
+          v-for="info in infos"
+          v-bind:key="info.title.toString()"
+          @click="handleClick(info.id)">
+        <span class="title">{{info.title}}</span>
+        <span class="date">{{info.date}}</span>
+      </li>
     </ul>
   </div>
 </template>
@@ -29,6 +35,36 @@ export default {
   },
 }; </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .list {
 
+    .item {
+      height: 50px;
+      line-height: 50px;
+      width: 50%;
+      min-width: 400px;
+      margin: 0 auto 10px auto;
+      /*margin-bottom: 10px;*/
+      background-color: #F9F9F9;
+      text-align: center;
+      border-left: 5px solid #52C08E;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #EFEFEF;
+      }
+
+      .title {
+        display: inline-block;
+        width: 250px;
+        font-weight: 500;
+        text-align: left;
+      }
+
+      .date {
+        font-size: 14px;
+        font-style: italic;
+      }
+    }
+  }
 </style>
